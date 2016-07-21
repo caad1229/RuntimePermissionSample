fbdaa42894e2969013ec89c45aa942549eb6e941
S.S <caad1229@gmail.com>
2016-07-21 16:47:36 +0900

    add utils for runtime permission

---
+++ app/src/main/java/com/caad1229/sample/runtimepermissionsample/utils/RuntimePermissionUtils.java
@@ -0,0 +1,37 @@
+package com.caad1229.sample.runtimepermissionsample.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by s.s on 2016/07/21.
+ */
+public class RuntimePermissionUtils {
+    private RuntimePermissionUtils() {
+    }
+
+    public static boolean hasSelfPermissions(@NonNull Context context, @NonNull String... permissions) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        for (String permission : permissions) {
+            if (context.checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkGrantResults(@NonNull int... grantResults) {
+        if (grantResults.length == 0) throw new IllegalArgumentException("grantResults is empty");
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
