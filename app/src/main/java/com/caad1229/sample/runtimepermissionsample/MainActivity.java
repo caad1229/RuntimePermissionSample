1c821aed78c480186b52ee37cb37af45553a4e98
S.S <caad1229@gmail.com>
2016-07-21 16:43:20 +0900

    impl launch camera

--- app/src/main/java/com/caad1229/sample/runtimepermissionsample/MainActivity.java
+++ app/src/main/java/com/caad1229/sample/runtimepermissionsample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.caad1229.sample.runtimepermissionsample;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.launch_camera_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
+    }
+
+    void launchCamera() {
+        Intent intent = new Intent();
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        startActivityForResult(intent, 0);
     }
 }
